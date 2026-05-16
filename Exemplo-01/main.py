a061336b04fffb92abc5d8ca32af3a3d6f95c010
Luciano Cordeiro <lacsousa@gmail.com>
2026-05-16 11:54:25 -0300

    feat: add GET and POST endpoints for performing addition calculations using path parameters and Pydantic models

--- Exemplo-01/main.py
+++ Exemplo-01/main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "from exemplo-01!"}
+
+# Passando o número 1 e 2 na URL
+@app.get("/soma/{numero1}/{numero2}")
+def soma(numero1: int, numero2: int):
+    total = numero1 + numero2
+    return {"resultado": total}
+
+
+class SomaRequest(BaseModel):
+    numero1: int
+    numero2: int
+
+# Passando o número 1 e 2 no corpo da requisição
+@app.post("/soma_formato2")
+def soma_formato2(dados: SomaRequest):
+    total = dados.numero1 + dados.numero2
+    return {"resultado": total}
